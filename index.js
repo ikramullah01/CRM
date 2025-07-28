a1e399458ff3c162dba537aea949a92a8e17e005
ikramullah01 <ikramullah0024@gmail.com>
2025-07-28 22:11:16 +0400

    database setup

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+import routes from "./src/routes/crmRoutes"
+
+
+const app = express();
+const PORT = 3000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb',{
+    useNewUrlParser: true
+});
+
+
+routes(app)
+
+app.get("/", (req,res) => {
+    res.send(`Node and express server is up and running on ${PORT}`);
+});
+
+app.listen(PORT,() => 
+    console.log(`Your server is running on port ${PORT}`)
+);
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,27 @@
+const routes = (app) => {
+    app.route('/contact')
+
+    .get((req,res,next) => {
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request type ${req.method}`)
+        next()
+    }, (req,res,next) => {
+        res.send("GET contact")
+    })
+
+    .post((req,res) => {
+        res.send("POST contact")
+    })
+
+    app.route('/contact/:contactId')
+
+    .put((req,res) => {
+        res.send("PUT contact")
+    })
+
+    .delete((req,res) => {
+        res.send("Delete contact")
+    })
+}
+
+export default routes;
